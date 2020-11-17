97687b10efdfa98b41b152f8bb3664e92501e821
Matthew Wei <matthewwei35@gmail.com>
2020-11-16 17:01:08 -0800

    Header component built

---
+++ src/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Title.css';
+
+function Title() {
+    return (
+        <div className="Title">
+            <header>
+                <h1>SFPOPOS</h1>
+                <div className="Title-Subtitle">San Francisco Privately Owned Public Open Spaces</div>
+            </header>
+        </div>
+    )
+}
+
+export default Title;
